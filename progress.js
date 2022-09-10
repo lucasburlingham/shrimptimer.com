ce8884837c81e7de0ca098bc8961cd9fa8778393
Lucas Burlingham <lucas@lucasburlingham.me>
2022-09-10 08:44:30 -0400

    Modal and responsiveness

--- progress.js
+++ progress.js
@@ -1,34 +1,22 @@
-const timeCreated = 1662591816;
-var hitTime = 1662609316;
+const timeCreated = 1662672921;
+var hitTime = 1662680021;
 var currentTime = Math.floor(Date.now() / 1000);
 
-
-// var timer = window.setInterval(computePercentage(hitTime, timeCreated), 1000);
-// console.log(timer);
-
-
 window.addEventListener('load', function () {
-	var timer = window.setInterval(computePercentage(hitTime, timeCreated), 1000);
-	var currentTime = Math.floor(Date.now() / 1000);
-	var percentage = Math.floor((hitTime - currentTime) / (hitTime - timeCreated) * 100) + "%";
-	console.log(percentage);
-	console.log("Width: " + document.getElementById("progressbar").style.width);
-	document.getElementById("progressbar").style.width = percentage;
-
+	const timer = window.setInterval(function () {
+		var currentTime = Math.floor(Date.now() / 1000);
+		var percentage = Math.floor((hitTime - currentTime) / (hitTime - timeCreated) * 100) + "%";
+		console.log(percentage);
+		// console.log("Width: " + document.getElementById("progressbar").style.width);
+		document.getElementById("progressbar").style.width = percentage;
+	}, 1000);
 });
 
 
-function computePercentage(hitTime, timeCreated) {
-
-	if (document.readyState === 'complete') {}
-
-	return percentage;
-}
-
-function endTimer() {
-	clearInterval(2);
+function endTimer(timer) {
+	clearInterval(timer);
+	var end_toast = document.getElementById("end_toast");
+	var myToast = new bootstrap.Toast(end_toast);
+	myToast.show();
 	console.log("Ending timer...");
 }
-
-
-console.log("hitTime: " + hitTime + " Current Time: " + currentTime + " Time Created: " + timeCreated);
